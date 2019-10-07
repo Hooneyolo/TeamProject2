8b8009046eaa0ce2d008a333f4b8dc22410c3fc1
YooJehun <dbwpgns0525@gmail.com>
2019-10-07 14:00:41 +0900

    Add Scanner  , fix reader

--- MyApp.java
+++ MyApp.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 /**
- * 여기에 MyApp 클래스 설명을 작성하십시오.
+ * txt 파일을 FileReader Class , Scanner 를 이용하여 hashmap에 저장후 Iterator 를 사용하여 총점과 평균을 계산
  * 
  * @author ( 2018315005 유제훈/ 2018315057 마츠바라 케이토 ) 
  * @version ( 2019/10/07 )
@@ -11,7 +11,8 @@ public class MyApp
     public static void main(String [] args){
         try{
         Scanner scanner = new Scanner(new FileReader("c:\\temp\\inputData20191007.txt"));
-        
+        HashMap<String, Integer> hash_map = new HashMap<String, Integer>();
+        hash_map.put = (,);
         // FileReader fr = new FileReader("c:\\temp\\inputData20191007.txt");
         // HashMap<String, Integer> hash_map = new HashMap<String, Integer>();
         // hash_mpa.put = (name, value);
